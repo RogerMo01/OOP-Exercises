ba7290df5bc9215abc31ee2ab713e93d4fd3e72f
Roger <morenoroger001@gmail.com>
2020-09-03 22:42:11 -0400

    feat: add Employee class

---
+++ OOP-Exercises/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercises
+{
+    class Employee
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Salary { get; private set; }
+
+        public Employee(int id, string firstName, string lastName, int salary)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public string GetFirstName()
+        {
+            return FirstName;
+        }
+
+        public string GetLastName()
+        {
+            return LastName;
+        }
+
+        public int GetSalary()
+        {
+            return Salary;
+        }
+        public void SetSalary(int newSalary)
+        {
+            Salary = newSalary;
+        }
+
+        public int GetAnualSalary()
+        {
+            return Salary * 12;
+        }
+
+        public void RaiseSalary(int percent)
+        {
+            Salary += Salary / 100 * percent;
+        }
+    }
+}
